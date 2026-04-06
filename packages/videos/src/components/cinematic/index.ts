93949d4b346de577ad9e7e57c21c034089789c65
Unite Group <zenithfresh25@gmail.com>
2026-04-07 07:26:06 +1000

    feat(video): add cinematic component barrel export

---
+++ packages/videos/src/components/cinematic/index.ts
@@ -0,0 +1,14 @@
+// packages/videos/src/components/cinematic/index.ts
+export { LetterboxReveal } from "./LetterboxReveal";
+export type { LetterboxRevealProps } from "./LetterboxReveal";
+
+export { KineticTextScene } from "./KineticTextScene";
+export type { KineticTextSceneProps, KineticBullet } from "./KineticTextScene";
+
+export { FullBleedScene } from "./FullBleedScene";
+export type { FullBleedSceneProps } from "./FullBleedScene";
+
+export { StatCounterScene } from "./StatCounterScene";
+export type { StatCounterSceneProps, StatItem } from "./StatCounterScene";
+
+export { CinematicCTA } from "./CinematicCTA";
