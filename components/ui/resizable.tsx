07427148d8a2e4233dbf26ac8d02a16d86aac9ed
Phill McGurk <phillmcgurk@Phills-MacBook-Pro.local>
2026-05-28 09:52:06 +1000

    fix: support resizable panels v4 exports

--- components/ui/resizable.tsx
+++ components/ui/resizable.tsx
@@ -6,17 +6,71 @@ import * as ResizablePrimitive from "react-resizable-panels";
 
 import { cn } from "@/lib/utils";
 
+type ResizablePrimitiveComponent = React.ComponentType<Record<string, unknown>>;
+
+type ResizablePrimitiveExports = Record<string, unknown> & {
+  Group?: ResizablePrimitiveComponent;
+  PanelGroup?: ResizablePrimitiveComponent;
+  Separator?: ResizablePrimitiveComponent;
+  PanelResizeHandle?: ResizablePrimitiveComponent;
+};
+
+const resizablePrimitives =
+  ResizablePrimitive as unknown as ResizablePrimitiveExports;
+const usesNextResizablePrimitives = Boolean(resizablePrimitives.Group);
+
+function getResizablePrimitive(
+  nextName: keyof ResizablePrimitiveExports,
+  legacyName: keyof ResizablePrimitiveExports,
+) {
+  const component =
+    resizablePrimitives[nextName] ?? resizablePrimitives[legacyName];
+
+  if (!component) {
+    throw new Error(
+      `react-resizable-panels is missing ${String(nextName)} / ${String(
+        legacyName,
+      )}`,
+    );
+  }
+
+  return component as ResizablePrimitiveComponent;
+}
+
+const ResizableGroupPrimitive = getResizablePrimitive("Group", "PanelGroup");
+
+const ResizableSeparatorPrimitive = getResizablePrimitive(
+  "Separator",
+  "PanelResizeHandle",
+);
+
+type ResizablePanelGroupProps = React.HTMLAttributes<HTMLDivElement> & {
+  autoSaveId?: string | null;
+  direction?: "horizontal" | "vertical";
+  keyboardResizeBy?: number | null;
+  onLayout?: (sizes: number[]) => void;
+  orientation?: "horizontal" | "vertical";
+};
+
 function ResizablePanelGroup({
   className,
+  direction,
+  orientation,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelGroup>) {
+}: ResizablePanelGroupProps) {
+  const resolvedOrientation = orientation ?? direction ?? "horizontal";
+
   return (
-    <ResizablePrimitive.PanelGroup
+    <ResizableGroupPrimitive
       data-slot="resizable-panel-group"
+      data-panel-group-direction={resolvedOrientation}
       className={cn(
         "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
         className,
       )}
+      {...(usesNextResizablePrimitives
+        ? { orientation: resolvedOrientation }
+        : { direction: resolvedOrientation })}
       {...props}
     />
   );
@@ -32,11 +86,11 @@ function ResizableHandle({
   withHandle,
   className,
   ...props
-}: React.ComponentProps<typeof ResizablePrimitive.PanelResizeHandle> & {
+}: React.HTMLAttributes<HTMLDivElement> & {
   withHandle?: boolean;
 }) {
   return (
-    <ResizablePrimitive.PanelResizeHandle
+    <ResizableSeparatorPrimitive
       data-slot="resizable-handle"
       className={cn(
         "bg-border focus-visible:ring-ring relative flex w-px items-center justify-center after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 focus-visible:ring-1 focus-visible:ring-offset-1 focus-visible:outline-hidden data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:translate-x-0 data-[panel-group-direction=vertical]:after:-translate-y-1/2 [&[data-panel-group-direction=vertical]>div]:rotate-90",
@@ -49,7 +103,7 @@ function ResizableHandle({
           <GripVerticalIcon className="size-2.5" />
         </div>
       )}
-    </ResizablePrimitive.PanelResizeHandle>
+    </ResizableSeparatorPrimitive>
   );
 }
 
